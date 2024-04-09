0d3764f23d00b78b15f29bcf0165192e155ea49e
blobstation <ghanshyam.rao@blobstation.com>
2024-04-09 10:55:16 +0530

    new section added

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
-window.addEventListener("load", enableMobileMenu);
-window.addEventListener("resize", enableMobileMenu);
-
-function enableMobileMenu() {
-  let menu = document.querySelector(".navbar.container"),
-    links = document.querySelector(".menu-inner.links");
-  if (window.innerWidth <= 1024) {
-    menu.addEventListener("click", () => {
-      menu.classList.toggle("opened");
-      links.classList.toggle("show");
-    });
-  } else {
-    if (menu.classList.contains("opened")) menu.classList.remove("opened");
-    if (links.classList.contains("show")) links.classList.remove("show");
-  }
+function showSidebar(){
+  const sidebar = document.querySelector('.sidebar')
+  sidebar.style.display = 'flex'
+}
+function hideSidebar(){
+  const sidebar = document.querySelector('.sidebar')
+  sidebar.style.display = 'none'
 }
